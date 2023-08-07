122af84fd20f701963d08128cc5a0b98af8a2e1e
adharsh.suresh <adharshsuresh1997@gmail.com>
2023-08-07 23:42:40 +0530

    till ep 03 completed

--- App.js
+++ App.js
@@ -1,44 +1,85 @@
-// let items = [
-//     {
-//         id : 1,
-//         name : "apple"
-//     },
-//     {
-//         id : 2,
-//         name : "carrot"
-//     },
-//     {
-//         id : 3,
-//         name : "beetroot"
-//     }
-// ]
-// const innerList = items.map(elem => React.createElement("li", { key : elem.id }, elem.name))
-// const list = React.createElement("ul",{ class : "bg-green p-0"},innerList)
-// const heading = React.createElement("h1",{},"Hello World from React")
-// const container = React.createElement("div",{id : "container"},[heading,list])
-
-// console.log(container)
+import React from "react"
+import ReactDOM from "react-dom/client"
 
 
-
-const newContainer = React.createElement("div",{id : "parent"},
-[
-    React.createElement("div",{id : "child"},[
-        React.createElement("h1",{},"I'm a h1 tag"),
-        React.createElement("h2",{},"I'm a h2 tag")
-    ]),
-    React.createElement("div",{id : "child2"},[
-        React.createElement("h1",{},"I'm a h1 tag"),
-        React.createElement("h2",{},"I'm a h2 tag")
-    ])
-]
-)
-
+const heading = React.createElement("h1",{id:"heading"},"Namaste React 🚀")
 const root = ReactDOM.createRoot(document.getElementById("root"))
-const root2 = ReactDOM.createRoot(document.getElementById("root2"))
 
-console.log(root)
+//react element
+const heading2 = (<>
+<h1 id="heading"> 
+Namaste React using JSX 🚀
+</h1>
+<h2>another one yeah</h2></>)
 
-//to delay the render
-setTimeout(() => {root.render(newContainer)} ,3000)
-setTimeout(() => {root2.render(newContainer)} ,5000)
+const Title  = () => {
+    return (<span style={{fontWeight:600,display:"block"}}>
+        This is title
+    </span>)
+}
+
+const AnotherHeading = () => {
+    return (
+    <div>
+    <h1>Namaste React Functional Component {100+200} {console.log('abcdefgh')}</h1>
+    {heading2}
+    {Title()}
+    <Title ></Title>
+    <Title />
+    </div>
+    )
+}
+
+console.log({heading})
+console.log({heading2})
+
+root.render(<AnotherHeading /> )
+
+
+
+// // let items = [
+// //     {
+// //         id : 1,
+// //         name : "apple"
+// //     },
+// //     {
+// //         id : 2,
+// //         name : "carrot"
+// //     },
+// //     {
+// //         id : 3,
+// //         name : "beetroot"
+// //     }
+// // ]
+// // const innerList = items.map(elem => React.createElement("li", { key : elem.id }, elem.name))
+// // const list = React.createElement("ul",{ class : "bg-green p-0"},innerList)
+// // const heading = React.createElement("h1",{},"Hello World from React")
+// // const container = React.createElement("div",{id : "container"},[heading,list])
+
+// // console.log(container)
+
+
+
+// const newContainer = React.createElement("div",{id : "parent"},
+// [
+//     React.createElement("div",{id : "child", key : 1},[
+//         React.createElement("h1",{key : 1},"This is namaste react "),
+//         React.createElement("h2",{key : 2},"I'm a h2 tag")
+//     ]),
+//     React.createElement("div",{id : "child2", key : 2},[
+//         React.createElement("h1",{key : 1},"I'm a h1 tag"),
+//         React.createElement("h2",{key : 2},"I'm a h2 tag")
+//     ])
+// ]
+// )
+// console.log(newContainer)
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// const root2 = ReactDOM.createRoot(document.getElementById("root2"))
+
+// console.log(root)
+
+// //to delay the render
+// // setTimeout(() => {root.render(newContainer)} ,3000)
+// // setTimeout(() => {root2.render(newContainer)} ,5000)
+// root.render(newContainer)
+// root2.render(newContainer)
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Namaste React 🚀
+
+
+# Parcel
+- Dev build
+- Local server
+- HMR = Hot Module Replacement using File watching algrothm (this algo is written in c++)
+- Faster Builds using Caching (see the .parcel-cache folder)
+- Image optimization
+- Minification (for production build)
+- Bundling
+- Compress
+- Consistent Hashing
+- Code splitting
+- Differential Bundling - support for older browsers
+- Error handling
+- Https
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
